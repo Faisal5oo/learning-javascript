7f539ef55c8d688934d387e7519e251ac0454371
Faisal5oo <133655119+Faisal5oo@users.noreply.github.com>
2023-06-19 18:00:09 +0500

    DONE

--- wordCounter.js
+++ wordCounter.js
@@ -1,12 +1,11 @@
 //---------------Word Counter----------------
 module.exports = (str) => {
   let characters = str.length;
-  console.log(`Number of characters in a string is ${characters}`);
   let words = str.split(" ").length;
-  console.log(` Numbers of words is ${words}`);
   let spaceRemover = str.trimEnd();
-  console.log(`${spaceRemover}`);
   let splited = str.replace(/ +/g, "");
-
+  console.log(`Number of characters in a string is ${characters}`);
+  console.log(` Numbers of words is ${words}`);
+  console.log(`${spaceRemover}`);
   console.log(`words without extra spaces ${splited.length}`);
 };
